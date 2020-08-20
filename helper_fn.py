0ffed6b450e341913b2d5694c8bc18cddf4ad5d4
gitwit-mistry <69296850+gitwit-mistry@users.noreply.github.com>
2020-08-20 17:52:53 +0530

    Add files via upload

---
+++ helper_fn.py
@@ -0,0 +1,43 @@
+import datetime
+import time
+import pyttsx3
+feed_back_list = []
+import wikipedia
+
+def time():
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
+
+
+def today_date():
+    today = datetime.date.today()
+    return today
+
+
+def stopwatch():
+    watch = int(input('for how many seconds\t'))
+    counter = watch
+    n = 0
+    while watch>n-1:
+        print(counter)
+        pyttsx3.speak(counter)
+        n += 1
+        counter -= 1
+    pyttsx3.speak('Your time is up!!!')
+    
+    
+def feedback_fn():
+    feed_back = input('please enter your valuable input')
+    feed_back_list.append(feed_back)
+    
+    with open('f1.txt','w') as f:
+        f.write('\n'.join(feed_back_list))
+        
+        
+def wiki():
+    search = input('enter the word or phrase to search')
+    
+    result = wikipedia.summary(search, sentences = 2)
+    
+    return result
---
+++ menu-prog.py
@@ -0,0 +1,80 @@
+import os
+import pyttsx3
+import datetime
+import time
+import helper_fn
+
+pyttsx3.speak('Welcome to your miniature menu of an operating system')
+
+while True:
+    p = input('what can i do for you?')
+    if ('run' in p or 'open') and 'chrome' in p:
+        if ('dont' in p or 'do not' in p):
+            print('Okay....i wont open it for you =\ ')
+        else:    
+            os.system('chrome')
+    if ('run' in p or 'open') and ('notepad' in p) or ('editor' in p):
+        if ('dont' in p or 'do not' in p):
+            print('Okay....i wont open it for you =\ ')
+        else:    
+            os.system('notepad')
+    if ('run' in p or 'open') and ('media' in p) or ('player' in p) or ('windows media player' in p):
+        if ('dont' in p or 'do not' in p):
+            print('Okay....i wont open it for you =\ ')
+        else:    
+            os.system('wmplayer')
+    if ('run' in p or 'open') and ('notebook' in p) or ('jupyter' in p):
+        if ('dont' in p or 'do not' in p or "don't" in p):
+            print('Okay....i wont open it for you =\ ')
+        else:    
+            os.system('jupyter notebook')
+    if ('run' in p or 'open') and ('spotify' in p):
+        if ('dont' in p or 'do not' in p or "don't" in p):
+            print('Okay....i wont open it for you =\ ')
+        else:    
+            os.system('spotify')
+    
+    if ('time' in p) and ('now' in p):
+        if ('dont' in p or 'do not' in p or "don't" in p):
+            print('Okay....i wont show it to you =\ ')
+        else:    
+            time_now = helper_fn.time()
+            pyttsx3.speak(time_now)
+            print(time_now)
+            
+    if ('date' in p):
+        if ('dont' in p or 'do not' in p or "don't" in p):
+            print('Okay....i wont show it to you =\ ')
+        else:    
+            today = helper_fn.today_date()
+            pyttsx3.speak("Today's Date is {}".format(today))
+            print("Today's Date is",today)
+            
+    
+    if ('run' in p or 'open') and (('stopwatch' in p) or ('timer' in p)):
+        if ('dont' in p or 'do not' in p or "don't" in p):
+            print('Okay....i wont  start timer for you =\ ')
+        else:    
+            helper_fn.stopwatch()
+            
+    if ('run' in p or 'open' in p or 'search' in p) and ('wikipedia' in p):
+        if ('dont' in p or 'do not' in p or "don't" in p):
+            print('Okay....i wont open it for you =\ ')
+        else:    
+            result = helper_fn.wiki()       
+    
+    
+    
+    if 'break' in p or 'quit' in p:
+        pyttsx3.speak('Goodbye and have a good day')
+        break
+        
+        
+    else :
+        print("Invalid Function or maybe the function is under development....")
+        print('Do you want to write a feed back to me so that i can improve?')
+        y_n = input('if yes press y else press any other key')
+        while y_n == 'y':
+            helper_fn.feedback_fn()
+            print('thank you for you feedback')
+        
